ce539fc982539754034c32a89b1243c0b835fd4c
TruongNM <truong2ck50@gmail.com>
2021-03-15 09:37:09 +0700

    update css banner

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -3,13 +3,13 @@
 @section('content')
     <div class="container">
         @if($slide)
-        <section class="hero pb-3 bg-cover bg-center d-flex align-items-center" style="background: url({{pare_url_file($slide->s_banner)}})">
+        <section class="hero pb-3 bg-cover bg-center d-flex align-items-center" style ="background: url('{{pare_url_file($slide->s_banner)}}')">
             <div class="container py-5">
                 <div class="row px-4 px-lg-5">
                     <div class="col-lg-6">
                         <p class="text-muted small text-uppercase mb-2">{{ $slide->s_description }}</p>
                         <h1 class="h2 text-uppercase mb-3">{{ $slide->s_title }}</h1>
-                        <a class="btn btn-dark" href="{{ $slide->s_link }}">{{ $slide->s_text}}</a>
+                        <a class="btn btn-dark" href="{{ $slide->s_link}}">{{ $slide->s_text}}</a>
                     </div>
                 </div>
             </div>
