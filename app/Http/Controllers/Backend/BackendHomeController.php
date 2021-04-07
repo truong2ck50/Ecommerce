46013d018bd44f310589ce24eb91b8165444bfe4
TruongNM <truong2ck50@gmail.com>
2021-04-07 14:05:53 +0700

    admin-dashboard

--- app/Http/Controllers/Backend/BackendHomeController.php
+++ app/Http/Controllers/Backend/BackendHomeController.php
@@ -3,12 +3,31 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Models\Product;
+use App\Models\Article;
+use App\Models\Transaction;
 use Illuminate\Http\Request;
 
 class BackendHomeController extends Controller
 {
     public function index() 
     {
-        return view('backend.index');
+        $countUser        = User::select('id')->count();
+        $countProduct     = Product::select('id')->count();
+        $countArticle     = Article::select('id')->count();
+        $countTransaction = Transaction::select('id')->count();
+        $transactions     = Transaction::orderByDesc('id')->limit(10)->get();
+        $users            = User::orderByDesc('id')->limit(10)->get();
+
+        $viewData   = [
+            'countUser'        => $countUser,
+            'countProduct'     => $countProduct,
+            'countArticle'     => $countArticle,
+            'countTransaction' => $countTransaction,
+            'transactions'     => $transactions,
+            'users'            => $users
+        ];
+        return view('backend.index', $viewData);
     }
 }
--- resources/views/backend/index.blade.php
+++ resources/views/backend/index.blade.php
@@ -1,4 +1,133 @@
 @extends('layouts.app_backend')
 @section('content')
-    <h1>Chào mừng bạn đến với hệ thống Admin</h1>
+    <div class="row">
+        <!-- Earnings (Monthly) Card Example -->
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                Thành viên
+                            </div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $countUser }}</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Earnings (Monthly) Card Example -->
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-success shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                Sản phẩm
+                            </div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $countProduct }}</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Earnings (Monthly) Card Example -->
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-info shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Đơn hàng
+                            </div>
+                            <div class="row no-gutters align-items-center">
+                                <div class="col-auto">
+                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">{{ $countTransaction }}</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Pending Requests Card Example -->
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-warning shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                                Bài viết
+                            </div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $countArticle }}</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-comments fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div  class="col-xl-4">
+            <h5>Thành viên mới đăng ký</h5>
+            <table class="table table-hover">
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($users as $item)    
+                    <tr>
+                        <td>{{ $item->id }}</td>
+                        <td>{{ $item->name }}</td>
+                        <td>{{ $item->email }}</td>
+                        <td>{{ $item->phone }}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+        <div class="col-xl-1"></div>
+        <div  class="col-xl-7">
+            <h5>Đơn hàng mới</h5>
+            <table class="table table-hover">
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Phone</th>
+                    <th>Total</th>
+                    <th>Note</th>
+                    <th>Time</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($transactions as $item)    
+                    <tr>
+                        <td>{{ $item->id }}</td>
+                        <td>{{ $item->t_name }}</td>
+                        <td>{{ $item->t_phone }}</td>
+                        <td><span class="text-danger">{{ number_format($item->t_total_money, 0, ',', '.') }} đ</span></td>
+                        <td>{{ $item->t_note }}</td>
+                        <td>{{ $item->created_at }}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 @stop
